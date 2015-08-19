379576d1a8dbdd023b92b6cb1d830b2fb905184c
Aurélien Luciani <aurelien.luciani@gmail.com>
2015-08-19 16:21:20 +1200

    more changes

--- data/space-plots.py
+++ data/space-plots.py
@@ -8,8 +8,8 @@ ax = fig.add_subplot(111, projection='3d')
 xs, ys, zs = zip(
   *[
     (x, y, z)
-    for x in np.arange(0, 1, 0.1)
-    for y in np.arange(3, 4.6, 0.2)
+    for x in np.arange(0, 1.1, 0.1)
+    for y in np.arange(3, 4.8, 0.2)
     for z in np.arange(0, 0.3, 0.05)
   ]
 )
@@ -19,6 +19,9 @@ ax.scatter(xs, ys, zs, c='b', marker='.')
 ax.set_xlabel('migrationProb')
 ax.set_ylabel('poissonMean')
 ax.set_zlabel('marriageThres')
+ax.set_xlim(0, 1.0)
+ax.set_ylim(3, 4.6)
+ax.set_zlim(0, 0.25)
 
 plt.show()
 
@@ -28,7 +31,10 @@ ax = fig.add_subplot(111, projection='3d')
 n = 5000
 xs = np.random.uniform(0, 1, n)
 ys = np.random.uniform(3, 4.6, n)
-zs = np.random.uniform(0, 0.3, n)
+zs = np.random.uniform(0, 0.25, n)
+ax.set_xlim(0, 1.0)
+ax.set_ylim(3, 4.6)
+ax.set_zlim(0, 0.25)
 
 ax.scatter(xs, ys, zs, c='b', marker='.')
 
